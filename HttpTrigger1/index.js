f44111733ad395d71e44fe2871919924fbcfc0bd
Samuel Whitby <samuel.whitby@thinkproject.com>
2022-03-16 16:37:22 +1300

    wait for ncp call to complete

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -2,18 +2,27 @@ const JsReport = require('jsreport')
 const path = require('path')
 const ncp = require('ncp')
 
-let jsreport
-const init = (async () => {    
-    jsreport = JsReport({
-        configFile: path.join(__dirname, '../', 'prod.config.json')
-    })    
-    await ncp(path.join(__dirname, '../', 'data'), '/tmp/data')
-    return jsreport.init()
-})()
+const init = (() => {
+    return new Promise((resolve, reject) => {        
+        ncp(path.join(__dirname, '../', 'data'), '/tmp/data', function (err) {
+            if (err) {
+              console.error(err);
+              return reject(err);
+            }
+            
+            const jsreport = JsReport({
+                configFile: path.join(__dirname, '../', 'prod.config.json')
+            })            
+            jsreport.init().then(() => {
+                resolve(jsreport);
+            })
+        })
+      });    
+})();
 
 module.exports = async function (context, req) {
     try {
-        await init
+        const jsreport = await init;
         // use the request body from post, if not specified return a sample template report for preview in browser
         const res = await jsreport.render(req.body || {
             template: {
